b13a4b06d53c10e0be922ff7d40a4a7076e34ac0
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2023-12-11 16:53:29 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,49 +4,57 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily  Upload limit 5GB
 	Price 0
 	
 	**VIP 1 ** 
-	Daily  Upload  limit 10GB
+	Daily  Upload  limit 20GB
 	Price Rs 55  🇮🇳/🌎 0.67$  per Month
 	
 	**VIP 2 **
 	Daily Upload limit 50GB
 	Price Rs 80  🇮🇳/🌎 0.97$  per Month
+
+        **VIP 3 **
+	Daily Upload limit **∞**GB
+	Price Rs 90  🇮🇳/🌎 0.97$  per Month
 	
-	Pay Using Upi I'd ```divindass@fbl```
+	Pay Using Upi I'd ```hxbots@pingpay```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/webx_admin_bot")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/webx_admin_bot"),
-        			InlineKeyboardButton("UPI ",url = "https://t.me/webx_admin_bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Kirodewal")], 
+        			[InlineKeyboardButton("UPI 🌎",url = "https://t.me/Kirodewal"),
+        			InlineKeyboardButton("PayTm ",url = "http://m.p-y.tm/requestPayment?recipient=8742089059")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily  Upload limit 5GB
 	Price 0
 	
 	**VIP 1 ** 
-	Daily  Upload  limit 10GB
+	Daily  Upload  limit 20GB
 	Price Rs 55  🇮🇳/🌎 0.67$  per Month
 	
 	**VIP 2 **
 	Daily Upload limit 50GB
 	Price Rs 80  🇮🇳/🌎 0.97$  per Month
+
+        **VIP 3 **
+	Daily Upload limit **∞**GB
+	Price Rs 90  🇮🇳/🌎 0.97$  per Month
 	
-	
-	Pay Using Upi I'd ```divindass@fbl```
+	Pay Using Upi I'd ```hxbots@pingpay```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/webx_admin_bot")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/webx_admin_bot"),
-        			InlineKeyboardButton("Paytm",url = "https://t.me/webx_admin_bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await message.reply_text(text = text,reply_markup = keybord)
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Kirodewal")], 
+        			[InlineKeyboardButton("UPI 🌎",url = "https://t.me/Kirodewal"),
+        			InlineKeyboardButton("PayTm ",url = "http://m.p-y.tm/requestPayment?recipient=8742089059")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
